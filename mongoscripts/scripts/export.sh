128e24d202f61371290870bbc56cc1dc2fd7db77
vaibhav <saini@manpho.(none)>
2013-04-28 19:42:16 -0700

    Export.sh changed..Added Train7

--- mongoscripts/scripts/export.sh
+++ mongoscripts/scripts/export.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
-# file_fields_path =/Users/vaibhavsaini/Documents/workspace/yelp/mongoscripts/scripts
-# output_path = /Users/vaibhavsaini/Documents/workspace/yelp
+#bibash
+# file_fields_path UservaibhavsainDocumentworkspacyelmongoscriptscripts
+# output_path =UservaibhavsainDocumentworkspacyelp
 #-- Unigrams---
-./mongoexport --db yelp_new --collection Unigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/unigrams/unigrams_freq.csv
-./mongoexport --db yelp_new --collection Food_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/unigrams/Food_Unigrams.csv
-./mongoexport --db yelp_new --collection Service_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/unigrams/Service_Unigrams.csv
-./mongoexport --db yelp_new --collection Price_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/unigrams/Price_Unigrams.csv
-./mongoexport --db yelp_new --collection Ambiance_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/unigrams/Ambience_Unigrams.csv
-./mongoexport --db yelp_new --collection Deal_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/unigrams/Deals_Unigrams.csv
+mongoexport --db $3 --collection Unigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpuunigramunigrams_freq.csv
+mongoexport --db $3 --collection Food_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpuunigramFood_Unigrams.csv
+mongoexport --db $3 --collection Service_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpuunigramService_Unigrams.csv
+mongoexport --db $3 --collection Price_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpuunigramPrice_Unigrams.csv
+mongoexport --db $3 --collection Ambiance_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpuunigramAmbience_Unigrams.csv
+mongoexport --db $3 --collection Deal_Unigrams --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpuunigramDeals_Unigrams.csv
 # -- bigrams----
-./mongoexport --db yelp_new --collection Bigrams_With_Freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/bigrams/Bigrams_With_Freq.csv
-./mongoexport --db yelp_new --collection Food_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/bigrams/Food_Bigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Service_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/bigrams/Service_Bigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Price_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/bigrams/Price_Bigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Ambiance_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/bigrams/Ambience_Bigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Deals_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/bigrams/Deals_Bigrams_with_freq.csv
+mongoexport --db $3 --collection Bigrams_With_Freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpubigramBigrams_With_Freq.csv
+mongoexport --db $3 --collection Food_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpubigramFood_Bigrams_with_freq.csv
+mongoexport --db $3 --collection Service_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpubigramService_Bigrams_with_freq.csv
+mongoexport --db $3 --collection Price_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpubigramPrice_Bigrams_with_freq.csv
+mongoexport --db $3 --collection Ambiance_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpubigramAmbience_Bigrams_with_freq.csv
+mongoexport --db $3 --collection Deals_Bigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outpubigramDeals_Bigrams_with_freq.csv
 # -- trigrams--
-./mongoexport --db yelp_new --collection Trigrams_With_Freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/trigrams/Trigrams_With_Freq.csv
-./mongoexport --db yelp_new --collection Food_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/trigrams/Food_Trigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Service_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/trigrams/Service_Trigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Price_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/trigrams/Price_Trigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Ambiance_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/trigrams/Ambience_Trigrams_with_freq.csv
-./mongoexport --db yelp_new --collection Deals_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/output/trigrams/Deals_Trigrams_with_freq.csv
+mongoexport --db $3 --collection Trigrams_With_Freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outputrigramTrigrams_With_Freq.csv
+mongoexport --db $3 --collection Food_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outputrigramFood_Trigrams_with_freq.csv
+mongoexport --db $3 --collection Service_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outputrigramService_Trigrams_with_freq.csv
+mongoexport --db $3 --collection Price_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outputrigramPrice_Trigrams_with_freq.csv
+mongoexport --db $3 --collection Ambiance_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outputrigramAmbience_Trigrams_with_freq.csv
+mongoexport --db $3 --collection Deals_Trigrams_with_freq --csv --fieldFile "$1/fields_unigrams.txt" --out $2/outputrigramDeals_Trigrams_with_freq.csv
 #-- combined--
 
 
